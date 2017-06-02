9ee0e86f4713fd4a21ad6ea1e1b1351a24a6a689
sido <sido@bicat.com>
2017-06-02 08:23:42 +0200

    CHORE(moved generic messages to special class)

---
+++ src/main/java/org/apache/maven/plugins/semver/exceptions/SemverExceptionMessages.java
@@ -0,0 +1,19 @@
+package org.apache.maven.plugins.semver.exceptions;
+
+/**
+ *
+ *
+ * <h1></h1>
+ *
+ *
+ * @author sido
+ */
+public class SemverExceptionMessages {
+
+  public static final String MESSAGE_ERROR_SCM_CREDENTIALS = "Please check your SCM-credentials to fix this issue";
+  public static final String MESSAGE_ERROR_PERFORM_ROLLBACK = "Please run semver:rollback to return to initial state";
+
+
+  private SemverExceptionMessages() {}
+
+}
